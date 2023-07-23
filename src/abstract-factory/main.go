9e6244ad80f6839890707f8a6c8c911b0782ef81
roosvelandia <roosvellc.velandia@gmail.com>
2023-07-23 11:14:54 -0500

    factory for SMS

---
+++ src/abstract-factory/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// SMS and email
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending Notification via SMS")
+}
+
+type SMSNotificationSender struct {
+}
+
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
+}
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+func main() {
+
+}
