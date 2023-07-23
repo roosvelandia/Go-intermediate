10f81d2d7892480356419eb822153050313db3a9
roosvelandia <roosvellc.velandia@gmail.com>
2023-07-23 10:20:30 -0500

    methods get and set

--- src/structs/class.go
+++ src/structs/class.go
@@ -7,9 +7,33 @@ type Employee struct {
 	name string
 }
 
+// method set id
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+// method set name
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+// method get id
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+// method get name
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	employee := Employee{id: 1, name: "Camilo"}
 	fmt.Println(employee)
-	employee.id = 2
+	employee.SetId(2)
 	fmt.Println(employee)
+	employee.SetName("Roosvell")
+	fmt.Println(employee)
+	fmt.Printf("mr. %v has %v id", employee.GetName(), employee.GetId())
+
 }
