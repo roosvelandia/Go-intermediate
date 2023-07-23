ed5325bc3abf59ce1b18f6f61181295926a3491d
roosvelandia <roosvellc.velandia@gmail.com>
2023-07-23 11:26:49 -0500

    factory design pattern

--- src/abstract-factory/main.go
+++ src/abstract-factory/main.go
@@ -33,6 +33,49 @@ func (SMSNotification) GetSender() ISender {
 	return SMSNotificationSender{}
 }
 
+// email
+type EmailNotification struct {
+}
+
+func (EmailNotification) SendNotification() {
+	fmt.Println("Sending Notification via Email")
+}
+
+type EmailNotificationSender struct {
+}
+
+func (EmailNotificationSender) GetSenderMethod() string {
+	return "Email"
+}
+func (EmailNotificationSender) GetSenderChannel() string {
+	return "SES"
+}
+func (EmailNotification) GetSender() ISender {
+	return EmailNotificationSender{}
+}
+
+func getNotificationFactory(notifitactionType string) (INotificationFactory, error) {
+	if notifitactionType == "SMS" {
+		return &SMSNotification{}, nil
+	}
+	if notifitactionType == "Email" {
+		return &EmailNotification{}, nil
+	}
+	return nil, fmt.Errorf("Not available")
+}
+func SendNotification(f INotificationFactory) {
+	f.SendNotification()
+}
+func getMehod(f INotificationFactory) {
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+
 func main() {
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+	_, error := getNotificationFactory("Voice")
+	SendNotification(smsFactory)
+	SendNotification(emailFactory)
+	fmt.Println(error)
 
 }
